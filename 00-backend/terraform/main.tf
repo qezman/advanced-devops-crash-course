ab8bccdabea1635aa3269306d08e0fe78b9e29b1
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-06-20 20:41:28 +0100

    fix: remove state lock

--- 00-backend/terraform/main.tf
+++ 00-backend/terraform/main.tf
@@ -41,20 +41,3 @@ resource "aws_s3_bucket_public_access_block" "tfstate" {
   ignore_public_acls = true
   restrict_public_buckets = true
 }
-
-# dynamodb table for state locking
-resource "aws_dynamodb_table" "tfstate_lock" {
-  name = "devops-course-tfstate-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name      = "devops-course-tfstate-lock"
-    ManagedBy = "terraform"
-  }
-}
--- 00-backend/terraform/outputs.tf
+++ 00-backend/terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "tfstate_bucket_name" {
   value = aws_s3_bucket.tfstate.id
 }
-
-output "tfstate_lock_table_name" {
-  value = aws_dynamodb_table.tfstate_lock.name
-}
