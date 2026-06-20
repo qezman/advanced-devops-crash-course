b6de8d310c9081b19cb43a3a02e4d8e210d68e30
Kazeem Jimoh <holaryinka5050@gmail.com>
2026-06-20 23:06:06 +0100

    fix: lockfile

--- 01-networking/terraform/provider.tf
+++ 01-networking/terraform/provider.tf
@@ -8,12 +8,13 @@ terraform {
     }
   }
 
+
   backend "s3" {
-    bucket         = "devops-course-tfstate-203637463799"
-    key            = "01-networking/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "devops-course-tfstate-lock"
-    encrypt        = true
+    bucket       = "devops-course-tfstate-203637463799"
+    key          = "01-networking/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
